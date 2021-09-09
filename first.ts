d82c5715bba9238b734df7be980e21b1de2c29ae
unknown <ebteshamhossain98@gmail.com>
2021-09-09 23:36:23 +0600

    first commit

---
+++ first.ts
@@ -0,0 +1,10 @@
+let money:number = 88;
+let honey:string = 'ezaz';
+
+function add(first:number, second:number):number{
+    const result = first + second;
+    return result;
+}
+
+const output: number = add(40, 87);
+
